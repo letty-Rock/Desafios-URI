99f7663bb9ad72ca4958d427e97b87846e64d6c4
Letícia Roque Lima Oliveira <62360470+letty-Rock@users.noreply.github.com>
2022-03-27 20:50:35 -0300

    Lesson1003

---
+++ Lesson1003.java
@@ -0,0 +1,13 @@
+public class Lesson1003 {
+    
+    public static void main(String[] args) {
+        int resultado, A, B;
+            A = 30;
+            B = 10;
+        
+                resultado = A + B;
+        
+        
+         System.out.println("Soma = " + resultado);
+         }
+}
