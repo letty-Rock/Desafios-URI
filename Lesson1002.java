a6eb02ad1a6f6bbe5f2b0a5386c03c2d81d9a841
Letícia Roque Lima Oliveira <62360470+letty-Rock@users.noreply.github.com>
2022-03-27 20:37:32 -0300

    1002

---
+++ Lesson1002.java
@@ -0,0 +1,11 @@
+public class Lesson1002 {
+ 
+    public static void main(String[] args) {
+    Double pi, raio, area;
+    pi = 3.14159;
+    raio = 2.00;
+    area = pi * (raio * raio);
+      System.out.println(" A area é: " +  area);
+    }
+ 
+}
