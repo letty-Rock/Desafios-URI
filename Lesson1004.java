23ab61af81b502181124757122f18e92f8c2d47f
Letícia Roque Lima Oliveira <62360470+letty-Rock@users.noreply.github.com>
2022-03-27 20:58:01 -0300

    1004

---
+++ Lesson1004.java
@@ -0,0 +1,10 @@
+public class Lesson1004 {
+    public static void main(String[] args) {
+        int A, B , PROD;
+        A = -30;
+        B = 10;
+        PROD = A * B;
+        
+        System.out.println("PROD = " + PROD);
+    }
+}
